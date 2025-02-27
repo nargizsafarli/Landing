3056cd76bffdc4c6989f1f6b60e90b6ca2426cd8
Nergiz Seferli <nergizsafarli60@gmail.com>
2025-02-28 02:17:02 +0400

    Section3 created

---
+++ src/Components/NavSection3/Section3.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./Section3.css"
+import sec3 from "./assets/image.png"
+
+function Section3() {
+  return (
+    <div className='sec3'>
+        <img src={sec3}/>
+        <div className='Text3'>
+            <h2>Light Fast & Powerful</h2>
+            <span>. Morbi ut quam id leo congue porttitor ac eget orci. Mauris porta fringilla ipsum non condimentum. In ac venenatis erat, nec hendrerit arcu. Donec tellus urna, cursus sit amet lorem eget, viverra pharetra mi.Duis sit amet interdum magna. Morbi feugiat ante condimentum lectus ullamcorper tincidunt. Proin consequat ante nibh, nec bibendum dui blandit id.</span>
+        </div>
+    </div>
+  )
+}
+
+export default Section3
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import "./Home.css"
 import Navbar from '../../Shared/Navbar/Navbar'
 import Introduce from '../../Components/Introduce/Introduce'
 import Section2 from '../../Components/NavSection2/Section2'
+import Section3 from '../../Components/NavSection3/Section3'
 
 
 function Home() {
@@ -11,6 +12,7 @@ function Home() {
         <Navbar/>
         <Introduce/>
        <Section2/>
+       <Section3/>
     </div>
   )
 }
